a0549eaba362124114dd12327332170462e73a51
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2022-12-07 13:54:15 +0530

    next-pwa modified

--- next.config.js
+++ next.config.js
@@ -1,30 +1,26 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  scope: '/',
+  sw: 'sw.js',
+  register: true,
+  skipWaiting: true,
+  fallbacks: {
+    image: '/images/wmds.jpg',
+    // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+  // runtimeCaching,
 })
 // const runtimeCaching = require('next-pwa/cache')
 
 
 const nextConfig = withPWA({
-  // pwa: {
-	// 	dest: 'public',
-  //   scope: '/',
-  //   sw: 'sw.js',
-  //   disable: false,
-  //   register: true,
-  //   skipWaiting: true,
-  //   fallbacks: {
-  //     image: '/images/wmds.jpg',
-  //     // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
-  //     // font: '/static/font/fallback.woff2',
-  //     // audio: ...,
-  //     // video: ...,
-  //   },
-  //   cacheOnFrontEndNav: true,
-  //   reloadOnOnline: true,
-  //   // runtimeCaching,
-	// },
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false, // disable powered by header.
